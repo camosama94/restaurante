bc40ed382e91cb54bb5b5f8ece1cc56ce8d07210
CarlosMolinaSánchez-Mateos <carlos94spain4@gmail.com>
2025-02-05 17:31:55 +0100

    Commit dia 05/02/2025 Introducción de archivos para autenticación y documentación con Swagger

---
+++ src/main/java/com/dwes/reserva/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.dwes.reserva.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Mi API").version("v1"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)  // Usar el tipo directamente
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
---
+++ src/main/java/com/dwes/reserva/repository/UserEntityRepository.java
@@ -0,0 +1,15 @@
+package com.dwes.reserva.repository;
+
+
+import com.dwes.reserva.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserEntityRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
+    Optional<UserEntity> findByEmail(String email);
+
+}
